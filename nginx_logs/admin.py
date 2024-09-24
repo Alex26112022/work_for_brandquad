ecd85fa3f61f1c1f6634030ba7a4d3652f9b16c5
Алексей Денисенко <AlexeyDenisenko2703@yandex.ru>
2024-09-24 14:37:54 +0300

    add model ParseLogs

---
+++ nginx_logs/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from nginx_logs.models import ParseLogs
+
+
+@admin.register(ParseLogs)
+class ParseLogsAdmin(admin.ModelAdmin):
+    list_display = (
+        'ip_address', 'date_log', 'method', 'url', 'status_code', 'size')
+    list_filter = ('method', 'status_code')
+    search_fields = ('ip_address', 'method', 'status_code')
+    list_display_links = (
+        'ip_address', 'date_log', 'method', 'url', 'status_code', 'size')
+    fields = ('ip_address', 'date_log', 'method', 'url', 'status_code', 'size')
