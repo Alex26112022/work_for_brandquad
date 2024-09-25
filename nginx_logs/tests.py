8d25c51e6fb928a5901203d8b6dc3b43690fd2c5
Алексей Денисенко <AlexeyDenisenko2703@yandex.ru>
2024-09-25 13:14:27 +0300

    add test

--- nginx_logs/tests.py
+++ nginx_logs/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from nginx_logs.models import ParseLogs
+
+
+class ParseLogsTestCase(APITestCase):
+    def setUp(self):
+        self.parse_logs = ParseLogs.objects.create(ip_address='127.0.0.1',
+                                                   date_log='2022-01-01 12:00:00',
+                                                   method='GET',
+                                                   uri='http://93.180.71.3/downloads/product_1',
+                                                   status_code=200,
+                                                   size=100)
+
+    def test_get_logs(self):
+        url = reverse('nginx_logs:logs_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        res = response.json()
+        self.assertEqual(res['results'][0]['ip_address'], '127.0.0.1')
